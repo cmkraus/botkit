fb07acb5ecd2ba5e04f7dc530a8a361f4895fbaf
cmkraus <cmkraus@us.ibm.com>
2016-07-07 14:34:56 -0400

    Update slack_bot.js

--- slack_bot.js
+++ slack_bot.js
@@ -25,7 +25,7 @@ This bot demonstrates many of the core features of Botkit:
 
   Run your bot from the command line:
 
-    token=<MY TOKEN> node slack_bot.js
+    token=<xoxb-57717923235-Rx9ciLnRrOqcxyhXBhpF6oIw> node slack_bot.js
 
 # USE THE BOT:
 
